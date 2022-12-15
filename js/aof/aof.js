9ed62001137526bed78b94ced84b73051535e9a8
sarfarazAnsarii <65850175+sarfarazAnsarii@users.noreply.github.com>
2022-12-15 09:51:33 +0530

    new changes

--- js/aof/aof.js
+++ js/aof/aof.js
@@ -10,8 +10,19 @@ $(document).mouseup(function(e)
 {
     var container = $("#dashProfile");    
     if (!container.is(e.target) && container.has(e.target).length === 0) 
-    {
-        
+    {        
         container.hide();
     }
+
+
+    var container = $(".slideDown");    
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {        
+        container.slideUp();
+    }
+
+});
+
+$("#stepsList").on("click", function(){
+    $("#kycStatus").slideToggle();
 });
